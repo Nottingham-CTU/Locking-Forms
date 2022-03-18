aa48d2cf6b924e7e91af8c0eb533c25188b22ead
Nina Clayton <nina.clayton@nottingham.ac.uk>
2022-03-18 12:29:18 +0000

    Implemented hard locks (full and intermediate DB locks)

---
+++ LockingButton.js
@@ -0,0 +1,152 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function removeLockControl()
+{
+    removeSaveControl();
+    var vListButton = $('input[type=button]');
+    for (var i = 0; i < vListButton.length; i++)
+    {
+        vLockBtn = vListButton[i];
+        if (vLockBtn.value == lang.data_entry_182)
+        { 
+            $('<i><b><br> Data hard locked</b></i>').insertBefore(vLockBtn);
+              vLockBtn.remove();
+
+        }
+    }  
+}
+
+function removeAddRecord()
+{
+   
+    var vListButton = $('button');
+    
+    for (var i = 0; i < vListButton.length; i++)
+    {
+        vAddBtn = vListButton[i];
+      
+        if (vAddBtn.innerText.trim() == lang.data_entry_46 || vAddBtn.innerText.trim() == 'Add new record' ||
+           vAddBtn.innerText.trim() == 'Add new record for the arm selected above' || vAddBtn.innerText.trim() == (lang.data_entry_46 + ' '+ lang.data_entry_442)
+           || vAddBtn.innerText.trim() == lang.data_entry_443 || vAddBtn.innerText.trim() == (lang.data_entry_443 + ' '+ lang.data_entry_442))
+        { 
+              vAddBtn.remove();
+        }
+        
+    }  
+}
+
+function removeSaveControl(locktext = '<b>Data hard locked</b>')
+{
+    var vListButton = $('button'); 
+    var bAddSaveText = true;
+    
+    for (var i = 0; i < vListButton.length; i++)
+    {
+        vSaveBtn = vListButton[i]; 
+        if (vSaveBtn.innerText.trim() == lang.data_entry_288)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext);
+ 
+        }
+        else if (vSaveBtn.innerText.trim() == lang.data_entry_409)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext);
+        }
+        else if (vSaveBtn.innerText.trim()== lang.data_entry_275)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext);  
+        }
+         else if (vSaveBtn.innerText.trim()== lang.data_entry_276)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext);  
+        }
+        else if (vSaveBtn.innerText.trim() == lang.data_entry_210)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext); 
+        }
+        else if (vSaveBtn.innerText.trim() == lang.data_entry_210)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext); 
+        }
+        else if (vSaveBtn.innerText.trim() == lang.data_entry_212)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext); 
+        }
+        else if (vSaveBtn.innerText.trim() == lang.data_entry_215)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext); 
+        }
+        else if (vSaveBtn.innerText.trim() == lang.data_entry_410)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext); 
+        }
+        else if (vSaveBtn.innerText.trim() == lang.data_entry_292)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext);
+        }
+        else if(vSaveBtn.innerText.trim() == lang.data_entry_289)
+        {
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext);
+        }
+        else if (vSaveBtn.innerText.trim() == lang.data_entry_234)
+        { 
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext);
+               
+        } 
+        else if (vSaveBtn.innerText.trim() == "Send SMS")
+        { 
+           removeButton(vSaveBtn, false, locktext);      
+        }
+        else if (vSaveBtn.innerText.trim() == "Send Email")
+        { 
+            removeButton(vSaveBtn, false, locktext);  
+        }
+        else if (vSaveBtn.innerText.trim() == lang.emlang_minimization_rando)
+        { 
+            removeButton(vSaveBtn, false, locktext);    
+        }
+       
+        else if (vSaveBtn.innerText.trim() == "Generate Letter")
+        { 
+            removeButton(vSaveBtn, false, locktext); 
+        }
+        else if(vSaveBtn.getAttribute('title') == lang.data_entry_287)
+        {
+            bAddSaveText = removeButton(vSaveBtn, bAddSaveText, locktext);
+        }
+       
+        
+    }
+      
+}
+
+function removeButton(vSaveBtn, bAddSaveText, locktext)
+{
+    if(bAddSaveText)
+    {
+        bAddSaveText = false;
+        $('<i><br> '+locktext+'</i>').insertBefore(vSaveBtn);
+    }
+    vSaveBtn.remove();
+    return bAddSaveText
+}
+
+// remove ranodise button if it exists
+$(document).ready(function(){
+   
+    var vButton = $('#redcapRandomizeBtn'); 
+    if(vButton != null)
+    {
+        vButton.remove();
+    }
+    removeAddRecord();
+    
+ });
+ 
+
+
+
--- README.md
+++ README.md
@@ -4,20 +4,29 @@ This REDCap module provides functionality to lock forms on saving.
 
 ## Project-level configuration options
 
-This module provides project-level configuration options, which are only visible to
-administrators.
+This module provides project-level configuration options, which are only visible to users with designer privileges.
+
+Settings for Hard locking
+* Full Hard Lock (only available for administrators(super users) to remove unlock and any save options for a form and options to add a new record.
+* Intermediate Hard Lock (only available for administrators(super users) to remove unlock and any save options for a form and options to add a new record.
+define all events and instrument that is EXEMPT in the event that should no longer editable
+	* Event name
+	* Instrument EXEMPT in event
+* Override Hard Lock	 (only available for administrators(super users) to override the removal of unlock button for administrators(super users) ONLY)
 
 Settings for Locking Instruments when saving a form with completed status:
 * Event name of saved instrument
 * Saved instrument
-
+* Do not allow new instances (only available for administrators(super users) to remove save options so new instance of repeating form or event cannot be added. The saved instrument must be locked).
 	for each saved instrument, define instruments to lock (multiple instruments can be locked on the saved) 
 	* Event name
-	* Instrument
+	* Instrument/ Instrument EXEMPT in event
 i.e. if the instrument wants to be locked on save, define instrument in save and lock section
 	
 Note: 
+* IT will receive an email if there are any data violations (data saved once a full or intermediate lock (hard lock) has been set regardless if override is checked or not.
 * If you do not want the form to lock while validation errors exist then use 'Validation Tweaker' external module and check the option, 'Require field validation to pass on form submission'.
 * If there are any data quality rule violation, the form will be reset to incomplete to allow these to be fixed or excluded. The form will need to be set to complete again to preform the locking on Save.
-* Only the same instance of the saving repeating forms and events can be locked 
+* Only the same instance of the saving of repeating form and repeating events can be locked. 
+  If the saving form is NOT a repeating form but a configured locking form is a repeating form, all instances of the repeatable form will be locked.
 
